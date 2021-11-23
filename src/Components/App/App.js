d1432120a7449a64d23fb6731c78f9d000260483
Aurimas-stack <rimas.zed@gmail.com>
2021-11-23 10:09:45 +0200

    final

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,14 +10,19 @@ class App extends React.Component {
       gameOn: false,
       gameReset: false,
       currentGameState: Array(1214).fill(0),
+      patterns:false,
     }
     this.handleColorSquare = this.handleColorSquare.bind(this);
     this.handleGameState = this.handleGameState.bind(this);
     this.handleResetAll = this.handleResetAll.bind(this);
     this.handleGameLogic = this.handleGameLogic.bind(this);
+    this.handleRandom = this.handleRandom.bind(this);
+    this.handleShowPatterns = this.handleShowPatterns.bind(this);
+    this.handleChoosePattern = this.handleChoosePattern.bind(this);
   }
   handleColorSquare = (event) => {
     const square = event.target.id;
+    console.log(square)
     if(this.state.currentGameState[Number(square)] === 0) {
       const gameValue = [...this.state.currentGameState]
       gameValue[Number(square)] = 1
@@ -104,7 +109,22 @@ class App extends React.Component {
       }))
     }
   }
-
+  handleRandom(event) {
+    const currState = [...this.state.currentGameState];
+    for(let i = 0; i < currState.length; i++) {
+      let randomNumb = Math.random();
+      if(randomNumb < 0.5) {
+        randomNumb = 0
+      } else {
+        randomNumb = 1
+      }
+      currState[i] = randomNumb;
+    }
+    this.setState({
+      currentGameState: currState
+    })
+    event.preventDefault();
+  }
   handleGameState = (event) => {
     const gameState = this.state.gameOn;
     if(gameState === false) {
@@ -127,6 +147,57 @@ class App extends React.Component {
     })
     event.preventDefault();
   }
+  handleShowPatterns = (event) => {
+    if(this.state.patterns === false) {
+      this.setState({
+        patterns: true
+      })
+    } else {
+      this.setState({
+        patterns:false
+      })
+    }
+    event.preventDefault();
+  }
+  handleChoosePattern = (event) => {
+    const currState = Array(1214).fill(0);
+    if(event.target.id === 'baby') {
+      currState[288] = 1; currState[333] = 1 ; currState[334] = 1; currState[379] = 1; currState[424] = 1;
+      currState[423] = 1; currState[422] = 1 ; currState[377] = 1; currState[468] = 1; currState[332] = 1;
+      this.setState({
+        currentGameState: currState
+      })
+    } else if(event.target.id === 'pulsar') {
+      currState[195] = 1; currState[240] = 1 ; currState[241] = 1; currState[245] = 1; currState[246] = 1;
+      currState[201] = 1; currState[156] = 1 ; currState[332] = 1; currState[331] = 1; currState[150] = 1;
+      currState[377] = 1; currState[375] = 1 ; currState[420] = 1; currState[421] = 1; currState[334] = 1;
+      currState[379] = 1; currState[335] = 1 ; currState[381] = 1; currState[426] = 1; currState[425] = 1;
+      currState[383] = 1; currState[338] = 1 ; currState[340] = 1; currState[339] = 1; currState[373] = 1;
+      currState[328] = 1; currState[326] = 1 ; currState[327] = 1; currState[510] = 1; currState[555] = 1;
+      currState[511] = 1; currState[557] = 1 ; currState[602] = 1; currState[601] = 1; currState[515] = 1;
+      currState[516] = 1; currState[561] = 1 ; currState[559] = 1; currState[604] = 1; currState[605] = 1;
+      currState[553] = 1; currState[596] = 1 ; currState[598] = 1; currState[553] = 1; currState[597] = 1; 
+      currState[563] = 1; currState[608] = 1; currState[609] = 1; currState[610] = 1; currState[691] = 1; 
+      currState[690] = 1; currState[735] = 1; currState[780] = 1; currState[695] = 1; currState[696] = 1; 
+      currState[741] = 1; currState[786] = 1; currState[515] = 1; currState[516] = 1; currState[561] = 1; 
+      currState[559] = 1; currState[604] = 1; currState[605] = 1;
+      this.setState({
+        currentGameState: currState
+      })
+    } else if(event.target.id === 'glider') {
+      currState[73] = 1; currState[116] = 1 ; currState[118] = 1; currState[151] = 1; currState[152] = 1;
+      currState[159] = 1; currState[160] = 1 ; currState[173] = 1; currState[174] = 1; currState[195] = 1;
+      currState[199] = 1; currState[229] = 1 ; currState[274] = 1; currState[275] = 1; currState[230] = 1;
+      currState[239] = 1; currState[284] = 1 ; currState[329] = 1; currState[375] = 1; currState[421] = 1;
+      currState[422] = 1; currState[245] = 1 ; currState[290] = 1; currState[335] = 1; currState[291] = 1;
+      currState[205] = 1; currState[204] = 1 ; currState[296] = 1; currState[298] = 1; currState[343] = 1;
+      currState[218] = 1; currState[219] = 1 ; currState[204] = 1; currState[250] = 1; currState[379] = 1;
+      currState[249] = 1; currState[288] = 1;
+      this.setState({
+        currentGameState: currState
+      })
+    }
+  }
   componentDidUpdate() {
     if(this.state.gameReset === true) {
       this.setState({
@@ -135,7 +206,7 @@ class App extends React.Component {
     }
   }
   componentDidMount() {
-      this.interval = setInterval(() => this.handleGenerations(), 150);
+    this.interval = setInterval(() => this.handleGenerations(), 150);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -168,6 +239,10 @@ class App extends React.Component {
           generation={this.state.generation} 
           gameOn={this.handleGameState}
           resetGame={this.handleResetAll}
+          random={this.handleRandom}
+          showPatterns={this.handleShowPatterns}
+          patterns={this.state.patterns}
+          choosePattern={this.handleChoosePattern}
         />
       </div>
     )
--- src/Components/GameOptions/GameOptions.js
+++ src/Components/GameOptions/GameOptions.js
@@ -1,21 +1,36 @@
 import './GameOptions.css';
 
-const GameOptions = ({generation, gameOn, resetGame}) => {
+const GameOptions = ({generation, gameOn, resetGame, random, showPatterns, patterns, choosePattern}) => {
     return (
         <div className='option-container'>
-            <div className='generation-counter option'>
-                <h2>Generation counter:</h2>
-                <p>{generation}</p>
+            {
+                patterns === false ?
+                <div>
+                <div className='generation-counter option'>
+                    <h2>Generation counter:</h2>
+                    <p>{generation}</p>
+                </div>
+                <div className='play-container option' onClick={gameOn}>
+                    <h2>Start/Pause/Resume</h2>
+                </div>
+                <div className='random option' onClick={random}>
+                    <h2>Randomize</h2>
+                </div>
+                <div className='clear-board option' onClick={resetGame}>
+                    <h2>Clear Board</h2>
+                </div>
+                <div className='patterns option' onClick={showPatterns}>
+                    <h2>Patterns</h2>
+                </div>
             </div>
-            <div className='play-container option' onClick={gameOn}>
-                <h2>Start/Pause/Resume</h2>
-            </div>
-            <div className='random option'>
-                <h2>Randomize</h2>
-            </div>
-            <div className='clear-board option' onClick={resetGame}>
-                <h2>Clear Board</h2>
-            </div>
+            :
+            <div className='pattern-options option'>
+                <h2 className='pattern-option' id='pulsar'onClick={choosePattern}>Pulsar</h2>
+                <h2 className='pattern-option' id='glider'onClick={choosePattern}>Glider Gun</h2>
+                <h2 className='pattern-option' id='baby' onClick={choosePattern}>Baby Pulsar</h2>
+                <h2 className='pattern-option' onClick={showPatterns}>Back</h2>
+            </div>            
+            }
         </div>
     )
 }
